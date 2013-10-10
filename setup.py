42e2c73b42446d1a46b81f9122fa5675de042c5f
John Stowers <john.stowers@gmail.com>
2013-10-10 15:26:02 +0200

    fix url

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(name='python-ros-electric' if not MY_PACKAGE else 'python-ros-electric-%s'
       description='Pure Python Ros (Electric)',
       author='John Stowers',
       author_email='john.stowers@gmail.com',
-      url='https://github.com/nzjrs/python-ros.git',
+      url='https://github.com/strawlab/ros-freeze.git',
       **get_disutils_cmds(srcdir, bindir, datadir)
  )
