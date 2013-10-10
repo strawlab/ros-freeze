50a8a5030c8c6a4c5a211636f36cfb492e81651f
John Stowers <john.stowers@gmail.com>
2013-10-10 13:32:09 +0200

    build rospy by default

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 from build import import_ros_core, import_ros_package, get_disutils_cmds
 
 srcdir, bindir, datadir = import_ros_core(".")
-import_ros_package(srcdir, bindir, datadir, "flycave", data='data')
+import_ros_package(srcdir, bindir, datadir, "rospy", data='data')
 
 setup(name='python-pyros',
       version='1.0',
