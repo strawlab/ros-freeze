e0f18d09296522e7e969370660206b6c2f51e377
John Stowers <john.stowers@gmail.com>
2013-10-10 21:27:03 +0200

    rename to rosfreeze

---
+++ setup-freezerospy.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+from rosfreeze import import_ros_core, import_ros_package, get_disutils_cmds
+
+MY_PACKAGE = ''
+
+if MY_PACKAGE:
+    srcdir, bindir, datadir = import_ros_package(MY_PACKAGE, data='data')
+else:
+    srcdir, bindir, datadir = import_ros_core()
+
+setup(name='python-ros-electric' if not MY_PACKAGE else 'python-ros-electric-%s' % MY_PACKAGE,
+      version='1.0',
+      description='Pure Python Ros (Electric)',
+      author='John Stowers',
+      author_email='john.stowers@gmail.com',
+      url='https://github.com/strawlab/ros-freeze.git',
+      **get_disutils_cmds(srcdir, bindir, datadir)
+ )
