3f22bab4ea889b6569d68977291cafcaba7267c5
John Stowers <john.stowers@gmail.com>
2013-10-10 15:18:08 +0200

    fix setup version

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ if MY_PACKAGE:
 else:
     srcdir, bindir, datadir = import_ros_core()
 
-setup(name='python-pyros' if not MY_PACKAGE else 'python-ros-%s' % MY_PACKAGE,
+setup(name='python-ros-electric' if not MY_PACKAGE else 'python-ros-electric-%s' % MY_PACKAGE,
       version='1.0',
-      description='Pure Python Ros',
+      description='Pure Python Ros (Electric)',
       author='John Stowers',
       author_email='john.stowers@gmail.com',
       url='https://github.com/nzjrs/python-ros.git',
